c4f6d1aef357368534471515d64e49b1beddcb3c
AndreCarneiro00 <andre.carneiro.silva00@gmail.com>
2026-04-17 09:49:36 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,46 +1,33 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
+import redisThread.RedisThread;
 
 public class Main {
-  public static void main(String[] args){
-    ServerSocket serverSocket = null;
-    Socket clientSocket = null;
-    int port = 6379;
-    try {
-      serverSocket = new ServerSocket(port);
-      System.out.printf("Listening to port %d%n", port);
-      // Since the tester restarts your program quite often, setting SO_REUSEADDR
-      // ensures that we don't run into 'Address already in use' errors
-      serverSocket.setReuseAddress(true);
+    public static void main(String[] args) {
+        ServerSocket serverSocket = null;
+        Socket clientSocket = null;
+        int port = 6379;
+        try {
+            serverSocket = new ServerSocket(port);
+            System.out.printf("Listening to port %d%n", port);
+            // Since the tester restarts your program quite often, setting SO_REUSEADDR
+            // ensures that we don't run into 'Address already in use' errors
+            serverSocket.setReuseAddress(true);
 
-      // Wait for connection from client.
-      clientSocket = serverSocket.accept();
-      System.out.println("Conection accepted");
+            while (true) {
+              // Wait for connection from client.
+              clientSocket = serverSocket.accept();
+              RedisThread redisThread = new RedisThread(clientSocket);
+              Thread thread = new Thread(redisThread);
+              System.out.println("Conection accepted");
 
-      InputStream input = clientSocket.getInputStream();
-      byte[] buffer = new byte[1024];
-      while (clientSocket.isConnected()) {
-        int readBytesCount = input.read(buffer);
-        OutputStream outputStream = clientSocket.getOutputStream();
-        String output = "+PONG\r\n";
-        outputStream.write(output.getBytes());
-        System.out.println("Wrote PONG");
-      }
+              thread.start();
+            }
 
 
-    } catch (IOException e) {
-      System.out.println("IOException: " + e.getMessage());
-    } finally {
-      try {
-        if (clientSocket != null) {
-          clientSocket.close();
+        } catch (IOException e) {
+            System.out.println("IOException: " + e.getMessage());
         }
-      } catch (IOException e) {
-        System.out.println("IOException: " + e.getMessage());
-      }
     }
-  }
 }
---
+++ src/main/java/redisThread/RedisThread.java
@@ -0,0 +1,40 @@
+package redisThread;
+
+import java.io.IOException;
+import java.net.Socket;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+
+public class RedisThread implements Runnable {
+    Socket clientSocket;
+
+    public RedisThread(Socket clientSocket) {
+        this.clientSocket = clientSocket;
+    }
+
+    public void run() {
+        try {
+            InputStream input = clientSocket.getInputStream();
+            while (clientSocket.isConnected()) {
+                byte[] buffer = new byte[1024];
+                int readBytesCount = input.read(buffer);
+                String inputString = new String(buffer).trim();
+                System.out.printf("inputString is %s%n", inputString);
+                OutputStream outputStream = clientSocket.getOutputStream();
+                outputStream.write("+PONG\r\n".getBytes());
+                System.out.println("Wrote PONG");
+            }
+        } catch (Exception e) {
+            System.out.println("Connection closed: " + e.getMessage());
+        } finally {
+            try {
+                if (clientSocket != null) {
+                    clientSocket.close();
+                }
+            } catch (IOException e) {
+                System.out.println("IOException: " + e.getMessage());
+            }
+        }
+    }
+}
