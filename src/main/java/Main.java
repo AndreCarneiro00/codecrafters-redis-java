a886f057e1d34293cdff8cb8f38c81aabe06d845
AndreCarneiro00 <andre.carneiro.silva00@gmail.com>
2026-04-17 10:47:55 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,4 @@
 import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -18,28 +16,11 @@ public class Main {
             while (true) {
               // Wait for connection from client.
               clientSocket = serverSocket.accept();
-                try {
-                    InputStream input = clientSocket.getInputStream();
-                    int readBytesCount;
-                    byte[] buffer = new byte[1024];
-                    while ((readBytesCount = input.read(buffer)) != -1) {
-                        String inputString = new String(buffer, 0, readBytesCount).trim();
-                        System.out.printf("inputString is %s%n", inputString);
-                        OutputStream outputStream = clientSocket.getOutputStream();
-                        outputStream.write("+PONG\r\n".getBytes());
-                        System.out.println("Wrote PONG");
-                    }
-                } catch (Exception e) {
-                    System.out.println("Connection closed: " + e.getMessage());
-                } finally {
-                    try {
-                        if (clientSocket != null) {
-                            clientSocket.close();
-                        }
-                    } catch (IOException e) {
-                        System.out.println("IOException: " + e.getMessage());
-                    }
-                }
+              RedisThread redisThread = new RedisThread(clientSocket);
+              Thread thread = new Thread(redisThread);
+              System.out.println("Conection accepted");
+
+              thread.start();
             }
         } catch (IOException e) {
             System.out.println("IOException: " + e.getMessage());
