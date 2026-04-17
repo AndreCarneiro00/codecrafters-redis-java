d7e7bdb35820974884224d1fa6d0d5e614224ad2
AndreCarneiro00 <andre.carneiro.silva00@gmail.com>
2026-04-17 10:22:04 -0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,6 +2,7 @@ import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
 
+
 public class Main {
     public static void main(String[] args) {
         ServerSocket serverSocket = null;
